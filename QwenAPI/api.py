d336560d914e5b56210641ecd9dd8fec77839fb7
wangchuan0225 <3266599343@qq.com>
2024-12-24 22:36:38 +0800

    完善了用户输入句子后得到关键词，通过提示词总结并联想合适关键词返回

--- QwenAPI/api.py
+++ QwenAPI/api.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 def call_api(message):
     client = OpenAI(
         # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
-        api_key="sk-68aa756d1aac410797b7adee53499e14",
+        api_key="sk",
         base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
     )
     completion = client.chat.completions.create(
